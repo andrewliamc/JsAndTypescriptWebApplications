c86575697b868a2ee129c931f804fb9b63080ba8
Andrew Liam C <andrewliamc@startmail.com>
2023-01-09 12:42:21 -0500

    Add imports and apply Routes
    Import React, Routes, Route, and all necessary components for App.js

--- instashare/instashare_frontend/src/App.js
+++ instashare/instashare_frontend/src/App.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { gapi } from 'gapi-script';
+import Home from './components/Home';
+import Login from './components/Login';
+
 
 const App = () => {
+  const navigate = useNavigate();
+  const clientId = process.env.REACT_APP_SANITY_PROJECT_ID;
+
   useEffect(() => {
     const initClient = () => {
         gapi.client.init({
@@ -12,10 +20,11 @@ const App = () => {
   });
 
   return (
-    <div>
-      Hello World.
-    </div>
-  )
-}
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
--- instashare/instashare_frontend/src/index.js
+++ instashare/instashare_frontend/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router} from 'react-router-dom';
 
-import app from './App';
+import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
