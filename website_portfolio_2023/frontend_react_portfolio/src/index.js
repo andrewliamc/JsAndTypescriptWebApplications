b0f31692c6c1271eccc665c38e880d1d614719e9
Andrew Liam C <andrewliamc@startmail.com>
2023-01-11 16:06:11 -0500

    Correct render on index.js
    Correct to RenderDOM to use correct import from react-dom

--- website_portfolio_2023/frontend_react_portfolio/src/index.js
+++ website_portfolio_2023/frontend_react_portfolio/src/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { ReactDOM } from 'react';
+import ReactDOM from 'react-dom';
 
 import App from './App';
 import './index.css';
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-root.render(
-  <Router>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { ReactDOM } from 'react';
+// import { createRoot } from "react-dom/client";
+// import { BrowserRouter as Router} from 'react-router-dom';
+
+// import App from './App';
+// import './index.css';
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+// root.render(
+//   <Router>
+//     <App />
+//   </Router>
+// );
