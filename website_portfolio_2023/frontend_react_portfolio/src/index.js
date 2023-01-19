74f9c9bfd0ec604c42d12f98ec4194797a6f4b52
Andrew Liam C <andrewliamc@startmail.com>
2023-01-19 16:09:44 -0500

    Change the render
    New convention for rendering in ReactJS 18 wasn't working, converted back to previous rendering convention.

--- website_portfolio_2023/frontend_react_portfolio/src/index.js
+++ website_portfolio_2023/frontend_react_portfolio/src/index.js
@@ -10,26 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import { ReactDOM } from 'react';
-// import { createRoot } from "react-dom/client";
-// import { BrowserRouter as Router} from 'react-router-dom';
-
-// import App from './App';
-// import './index.css';
-
-// const rootElement = document.getElementById("root");
-// const root = createRoot(rootElement);
-// root.render(
-//   <Router>
-//     <App />
-//   </Router>
-// );
